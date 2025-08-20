626e6a514bcb47739c192c835f3cb39abfd78bc3
unknown <mohameddacarmohumed@gmail.com>
2025-08-20 07:04:44 +0300

    cart page added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import Container from '../components/Container'
+import Image from '../components/Image'
+import Button from '../components/Button'
+import Cart1 from '../assets/cart1.png'
+import Flex from '../components/Flex'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <section className='py-[140px]'>
+      <Container>
+          <ul className ='flex gap-x-[340px] bg-[#F5F5F3] py-[34px] px-5'>
+            <li className='text-secondary text-xl font-dm font-bold'>Product </li>
+            <li className='text-secondary text-xl font-dm font-bold'>Price </li>
+            <li className='text-secondary text-xl font-dm font-bold'>Quantity</li>
+            <li className='text-secondary text-xl font-dm font-bold'>Total</li>
+          </ul>
+          <ul className ='flex items-center py-8 border-b border-[#F5F5F3]'>
+            <li className='px-3'>X </li>
+            <li className='px-10'><Image src={Cart1} /></li>
+            <li className='px-10 text-secondary text-base font-dm font-bold'>Product name</li>
+            <li className='px-10 text-secondary text-xl font-dm font-bold'>$44.00</li>
+            <li className='ml-[280px] border border-[#F0F0F0] px-7 py-2 flex gap-x-6 text-sm text-primary'><span>+</span><span>1</span><span>-</span></li>       
+            <li className='ml-[280px] text-secondary text-xl font-dm font-bold'>$44.00</li>
+          </ul>
+          <ul className='flex items-center gap-x-10 py-8 border-b border-[#F5F5F3]'>
+            <li className='py-2 px-12 border-b border-[#F5F5F3] text-secondary text-xl font-dm font-bold'>
+              <select>
+                <option value="">SIZE</option>
+                <option value="">M</option>
+                <option value="">L</option>
+                <option value="">S</option>
+              </select>
+            </li>
+            <li className='pl-[30px] text-secondary text-sm font-dm font-bold'>Apply coupon</li>
+            <li className='pl-[1000px] text-secondary text-sm font-dm font-bold'>Update cart</li>
+          </ul>
+          <h4 className='text-secondary text-xl font-bold font-dm text-end pt-[54px] pb-6'>Cart totals</h4>
+          <div className='w-[644px] border border-[#F5F5F3] ml-auto'>
+            <Flex className='border-b border-[#F5F5F3]'>
+              <p className='border-r border-[#F5F5F3] pl-5 pr-[200px] py-4 text-base text-secondary font-dm font-bold'>Subtotal</p>
+              <p className='pl-5 pr-[200px] py-4 text-base text-primary font-dm font-bold'>389.99 $</p>
+            </Flex>
+            <Flex className='border-b border-[#F5F5F3]'>
+              <p className='border-r border-[#F5F5F3] pl-5 pr-[200px] py-4 text-base text-secondary font-dm font-bold'>Total</p>
+              <p className='pl-5 pr-[200px] py-4 text-base text-primary font-dm font-bold'>389.99 $</p>
+            </Flex>
+          </div>
+          <div className='text-end pt-8'>
+            <Button text='Proceed to Checkout' />
+          </div>
+      </Container>
+    </section>
   )
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,162 @@
 import React from 'react'
-
+import Container from '../components/Container'
+import Heading from '../components/Heading'
+import Flex from '../components/Flex'
+import Button from '../components/Button'
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <section className='py-[130px]'>
+      <Container>
+        <div className='border-b border-[#F0F0F0] pb-[60px] mb-[60px]'>
+          <p className='text-primary text-[16px] font-dm font-normal w-[644px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+        </div>
+        <Heading className='' text='Your Personal Details'/>
+        <Flex className='gap-x-10 flex-wrap'>
+          <div className='w-[507px] mb-7 mt-10'>
+          <label className='text-base text-secondary font-dm font-bold' htmlFor="firstname">First Name: <br />
+            <input className='py-4 border-b border-[#F0F0F0] w-full' type="text" id='firstname' placeholder='First Name' />
+          </label>
+        </div>
+        <div className='w-[507px] mb-7 mt-10'>
+          <label className='text-base text-secondary font-dm font-bold' htmlFor="lastname">Last Name: <br />
+            <input className='py-4 border-b border-[#F0F0F0] w-full' type="password" id='text' placeholder='Last Name' />
+          </label>
+        </div>
+          <div className='w-[507px] mb-7 mt-10'>
+          <label className='text-base text-secondary font-dm font-bold' htmlFor="">Email Address: <br />
+            <input className='py-4 border-b border-[#F0F0F0] w-full' type="text" id='email' placeholder='company@domain.com' />
+          </label>
+        </div>
+        <div className='w-[507px] mb-7 mt-10'>
+          <label className='text-base text-secondary font-dm font-bold' htmlFor="telephone">Telephone: <br />
+            <input className='py-4 border-b border-[#F0F0F0] w-full' type="text" id='telephone' placeholder='Telephone' />
+          </label>
+        </div>
+        </Flex>
+        <div className='border-b border-[#F0F0F0] pb-[70px] mb-[52]]'>
+        </div>
+      <div className='pt-10'>
+        <Heading text='New Customer'/>
+        <Flex className='gap-x-10 flex-wrap'>
+          <div className='w-[507px] mb-7 mt-10'>
+          <label className='text-base text-secondary font-dm font-bold' htmlFor="address">Address 1: <br />
+            <input className='py-4 border-b border-[#F0F0F0] w-full' type="text" id='address' placeholder='123-123 Buuloxuubeey' />
+          </label>
+        </div>
+        <div className='w-[507px] mb-7 mt-10'>
+          <label className='text-base text-secondary font-dm font-bold' htmlFor="address2">Address 2: <br />
+            <input className='py-4 border-b border-[#F0F0F0] w-full' type="text" id='address2' placeholder='4279 Hammarweyne' />
+          </label>
+        </div>
+          <div className='w-[507px] mb-7 mt-10'>
+          <label className='text-base text-secondary font-dm font-bold' htmlFor="city">City: <br />
+            <input className='py-4 border-b border-[#F0F0F0] w-full' type="text" id='city' placeholder='Your City' />
+          </label>
+        </div>
+        <div className='w-[507px] mb-7 mt-10'>
+          <label className='text-base text-secondary font-dm font-bold' htmlFor="code">Post Code: <br />
+            <input className='py-4 border-b border-[#F0F0F0] w-full' type="text" id='code' placeholder='252' />
+          </label>
+        </div>
+        <br />
+                <div className="w-[507px] py-6">
+              <label
+                className="text-base text-secondary font-dm font-bold"
+                htmlFor="country"
+              >
+                City
+              </label>
+              <select
+                className="mt-2 block border-b border-[#F0F0F0] w-full text-sm text-primary font-dm font-normal py-2 focus:outline-none"
+                id="country"
+              >
+                <option value="01">Please select</option>
+                <option value="bangladesh">Bangladesh</option>
+                <option value="nepal">Nepal</option>
+                <option value="pakistan">Pakistan</option>
+              </select>
+            </div>
+
+
+        <div className="w-[507px] py-6">
+              <label
+                className="text-base text-secondary font-dm font-bold"
+                htmlFor="country"
+              >
+                Region/State
+              </label>
+              <select
+                className="mt-2 block border-b border-[#F0F0F0] w-full text-sm text-primary font-dm font-normal py-2 focus:outline-none"
+                id="country"
+              >
+                <option value="01">Please select</option>
+                <option value="bangladesh">Bangladesh</option>
+                <option value="nepal">Nepal</option>
+                <option value="pakistan">Pakistan</option>
+              </select>
+            </div>
+            <div className='border-b border-[#F0F0F0] pb-[70px] mb-[52]'>
+        </div>
+        </Flex>
+      </div>
+      <div className='border-b border-[#F0F0F0] pb-[70px] mb-[52]]'>
+        </div>
+      <div className='pt-10'>
+        <Heading text='Your Password'/>
+        <Flex className='gap-x-10 flex-wrap'>
+          <div className='w-[507px] mb-7 mt-10'>
+          <label className='text-base text-secondary font-dm font-bold' htmlFor="password">Password: <br />
+            <input className='py-4 border-b border-[#F0F0F0] w-full' type="password" id='password' placeholder='Your Password' />
+          </label>
+        </div>
+        <div className='w-[507px] mb-7 mt-10'>
+          <label className='text-base text-secondary font-dm font-bold' htmlFor="rppassword">Repeat Password: <br />
+            <input className='py-4 border-b border-[#F0F0F0] w-full' type="password" id='rppassword' placeholder='Repeat Password' />
+          </label>
+        </div>
+        </Flex>
+      </div>
+
+<div className='border-b border-[#F0F0F0] pb-[70px] mb-[52]]'>
+        </div>
+
+      <div className="relative mt-[65px]">
+        <label
+          className="ml-10 text-sm text-primary font-dm font-normal"
+          htmlFor="iagrre"
+        >
+          <input
+            className="pr-10 absolute top-1/2 left-0 -translate-y-1/2"
+            type="checkbox"
+            id="iagrre"
+          />
+          I have read and agree to the Privacy Policy
+        </label>
+      </div>
+  
+      <Flex>
+        <div className='pt-7'>
+          Subscribe Newsletter
+          <div className='relative inline ml-10'>
+          <label className='ml-4 text-sm text-primary font-dm font normal' For="yes">
+            <input className="pr-10 absolute top-1/2 right-7 -translate-y-1/2" type="checkbox"
+            id="iagrre" />
+            Yes
+          </label>
+        </div>
+        <div className='relative inline ml-10'>
+          <label className='ml-4 text-sm text-primary font-dm font normal' For="yes">
+            <input className="pr-10 absolute top-1/2 right-7 -translate-y-1/2" type="checkbox"
+            id="iagrre" />
+            Yes 
+          </label>
+        </div>
+        </div> 
+        
+      </Flex>
+      <Button className='mt-7' text='Log in' />
+      </Container>
+    </section>
   )
 }
 
